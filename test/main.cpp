760d3bc13f2d183c850391285e44819eee3ee5ac
Eric Menezes <eirc.m@protonmail.com>
2018-04-10 18:39:00 +0000

    Setup Google Tests framework for project's testing.

--- test/main.cpp
+++ test/main.cpp
@@ -1,35 +1,12 @@
-#include <iostream>
-#include <memory>
-#include <string.h>
-#include <vector>
+#include "gtest/gtest.h"
 
-#include "custom-proxy/ICustomInterface.hpp"
-#include "custom-proxy/QCustomProxy.hpp"
-#include "Jeplu.hpp"
-
-int main(int argc, const char **argv)
+// Test function
+bool testTrue(bool value)
 {
-    std::string pluginsPath;
-    pluginsPath += "plugins/";
+    return value;
+}
 
-    std::cout << "Searching for plugins in " << pluginsPath << std::endl;
-
-    Jeplu pluginManager;
-    std::shared_ptr<QCustomProxy> proxy = std::make_shared<QCustomProxy>();
-    pluginManager.registerProxy(proxy);
-    pluginManager.init(pluginsPath);
-
-    if (proxy->initialized())
-    {
-        for (std::weak_ptr<ICustomInterface> wPlugin : proxy->interfaces())
-        {
-            std::shared_ptr<ICustomInterface> plugin = wPlugin.lock();
-            if (plugin)
-            {
-                std::cout << "Loading custom function from plugin " << plugin->pluginName() << std::endl;
-                plugin->customFunction();
-            }
-        }
-    }
-    return 0;
+// Test assignment
+TEST(testTrue, TestTrue) {
+    EXPECT_EQ(true, testTrue(false));
 }
